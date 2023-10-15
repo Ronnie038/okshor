be3fd089120f9ee532e9f139132b209d7921b22d
morsaline <morsaline>
2023-10-16 03:03:20 +0600

    website size

--- client/src/Layouts/MainLayout.jsx
+++ client/src/Layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "../Shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="">
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
